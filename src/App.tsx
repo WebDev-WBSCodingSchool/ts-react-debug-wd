d708d1a0ab2d49249bea60bc573d9fd1163d798c
Jorge Paul <jserranopaul@gmail.com>
2025-07-04 16:23:14 +0200

    Readme

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,6 @@ import { Loading } from '@/components';
 import { getAllEvents } from '@/data';
 import { createEventAction } from '@/actions';
 
-let a = 1;
-a = 'test'; // This line is just to demonstrate that the code is being executed in a TypeScript environment.
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route element={<MainLayout />} hydrateFallbackElement={<Loading />}>
