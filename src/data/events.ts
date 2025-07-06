d0e64303b4df0162171f6c54c0a590a120d1875d
Jorge Paul <jserranopaul@gmail.com>
2025-07-07 00:29:00 +0200

    Error on missing API URL

--- src/data/events.ts
+++ src/data/events.ts
@@ -1,6 +1,10 @@
 const API_URL = import.meta.env.VITE_EVENTS_API_URL;
 
 export const getAllEvents = async ({ request }) => {
+  if (!API_URL)
+    throw new Error(
+      'Something tells me you forgot to set the VITE_EVENTS_API_URL environment variable.'
+    );
   const url = new URL(request.url);
   const page = url.searchParams.get('page') || '1';
   const limit = url.searchParams.get('limit') || '10';
--- src/data/homepage.ts
+++ src/data/homepage.ts
@@ -1,6 +1,10 @@
 const API_URL = import.meta.env.VITE_EVENTS_API_URL;
 
 export const getHomePageData = async () => {
+  if (!API_URL)
+    throw new Error(
+      'Something tells me you forgot to set the VITE_EVENTS_API_URL environment variable.'
+    );
   const usersPromise = fetch(`${API_URL}/users`);
   const eventsPromise = fetch(`${API_URL}/events`);
   const [resUsers, resEvents] = await Promise.all([usersPromise, eventsPromise]);
